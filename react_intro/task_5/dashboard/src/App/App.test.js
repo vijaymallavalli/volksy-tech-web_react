e14363ce377f036b208faba23342836906eb181f
vijaymallavalli <vijaysaradhim@brightcomgroup.com>
2023-04-24 11:12:13 +0000

    d

--- react_intro/task_5/dashboard/src/App/App.test.js
+++ react_intro/task_5/dashboard/src/App/App.test.js
@@ -1,19 +1,26 @@
-import React from 'react';
 import { shallow } from 'enzyme';
+import React from 'react';
 import App from './App';
 
-const wrapper = shallow(<App />);
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+describe('<App />', () => {
+	it('Tests that App renders without crashing', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.exists()).toBe(true);
+	})
 
-it('renders App-header', () => {
-  expect(wrapper.find('header.App-header').exists()).toEqual(true);
-});
-it('renders App-body', () => {
-  expect(wrapper.find('main.App-body').exists()).toEqual(true);
-});
-it('renders App-footer', () => {
-  expect(wrapper.find('footer.App-footer').exists()).toEqual(true);
+	it('Tests that App renders a <div> with class "App-header"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-header').length).toBe(1);
+	})
+
+	it('Tests that App renders a <div> with class "App-body"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-body').length).toBe(1);
+	})
+
+	it('Tests that App renders a <div> with class "App-footer"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-footer').length).toBe(1);
+	})
 });
--- react_intro/task_5/dashboard/src/Notifications/Notifications.js
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,40 +1,30 @@
-import React from 'react';
-import './Notifications.css';
-import close_icon from '../assets/close-icon.png';
-import { getLatestNotification } from '../utils/utils';
+import React from 'react'
+import close_icon from '../assets/close-icon.png'
+import { getLatestNotification } from '../utils/utils'
 
-const Notifications = () => {
+import './Notifications.css'
+
+export default function Notification(props) {
   return (
-    <div className='Notifications'>
-      <p>Here is the list of notifications</p>
-      <ul>
-        <li data-priority='default'>New course available</li>
-        <li data-priority='urgent'>New resume available</li>
-        <li data-priority='urgent'>
-          <div
-            dangerouslySetInnerHTML={{ __html: `${getLatestNotification()}` }}
-          ></div>
-        </li>
-      </ul>
-      <button
-        type='button'
-        aria-label='Close'
-        onClick={() => console.log('Close button has been clicked')}
-        style={{
-          display: 'inline-block',
-          position: 'absolute',
-          top: '16px',
-          right: '16px',
-          background: 0,
-          border: 0,
-          outline: 'none',
-          cursor: 'pointer',
-        }}
-      >
-        <img src={close_icon} alt='' style={{ width: '8px', height: '8px' }} />
-      </button>
-    </div>
-  );
-};
-
-export default Notifications;
+	<div className="Notifications">
+	  <button style={{ 
+		  position: 'absolute',
+		  background: 'transparent',
+		  border: 'none',
+		  right: '20px',
+		 }} 
+		 aria-label='close' 
+		 onClick={() => {
+		  console.log('Close button has been clicked');
+	  }}>
+	    <img src={close_icon} alt="close" height="15px" width="15px"></img>
+	  </button>
+	  <p>Here is the list of notifications</p>
+	  <ul>
+		  <li data-priority="default">New course available</li>
+		  <li data-priority="urgent">New resume available</li>
+	  	  <li dangerouslySetInnerHTML={getLatestNotification()}></li>
+	  </ul>
+	</div>
+  )
+}
